46e6c146b69c4bba0d5d72d93335ce1261c5f845
Gilbert Oguta <gilbertoguta37@gmail.com>
2024-08-26 17:06:17 +0300

    update

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+console.log('MongoDB URI:', uri); // Verifying the URI
+
+mongoose.connect(uri)
+  .then(() => {
+    console.log("MongoDB database connection established successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+  const exercisesRouter = require('./routes/exercises');
+  const usersRouter = require('./routes/users');
+  
+  app.use('/exercises', exercisesRouter);
+  app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
